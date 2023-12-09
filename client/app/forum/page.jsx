234af1429411d194487104f02462d177cfbd1f39
Sarthak <notsarthakshah@gmail.com>
2023-12-09 20:01:36 +0530

    starting proposals

--- client/app/forum/page.jsx
+++ client/app/forum/page.jsx
@@ -2,27 +2,37 @@
 import React from 'react';
 import Navbar from '../components/header/Navbar';
 import Card from './card/Card';
+import Image from 'next/image';
 
-const Page = () => {
+const page = () => {
   return (
-    <div className="bg-cover bg-center">
+    <div>
       <Navbar />
-      <div className='text-[65px] font-bold text-center mt-20 mb-6 leading-none'>
-        Step into 
-        <span className='relative inline-block pl-2 pr-2'>
-          <span className='text-green-400 drop-shadow-xl shadow-emerald-400 pl-2 pr-2'>zXDAO's</span>
-        </span>
-        Forum.
-      </div>
-      <div className='flex justify-center items-center text-[#a1a1aa]'>
-        The place where the latest news and proposals are discussed!
-      </div>
-      <div className='flex gap-5 justify-center pt-10'>
-        <Card text={"General"} description={"Discover and discuss a variety of topics"}/>
-        <Card text={"Proposals"} description={"Explore and contribute to innovative project ideas and suggestions"}/>
+      <div className="flex bg-cover bg-center">
+        {/* Left half */}
+        <div className="flex-1">
+          <div className='text-[65px] font-bold text-center mt-20 mb-6 leading-none'>
+            Step into 
+            <span className='relative inline-block pl-2 pr-2'>
+              <span className='text-green-400 drop-shadow-xl shadow-emerald-400 pl-2 pr-2'>zXDAO's</span>
+            </span>
+            Forum.
+          </div>
+          <div className='flex justify-center items-center text-[#a1a1aa]'>
+            The place where the latest news and proposals are discussed!
+          </div>
+          <div className='flex gap-8 justify-center pt-10'>
+            <Card text={"General"} description={"Discover and discuss a variety of topics"}/>
+            <Card text={"Proposals"} description={"Explore and contribute to innovative project ideas and suggestions"}/>
+          </div>
+        </div>
+        {/* Right half for image */}
+        <div className="flex-1 flex justify-center items-center gap-4">
+          <Image src="/hand.svg" width={450} height={450} responsive/>
+        </div>
       </div>
     </div>
   );
 };
 
-export default Page;
+export default page;
